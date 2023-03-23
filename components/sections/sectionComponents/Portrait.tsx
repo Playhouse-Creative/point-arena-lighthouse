7cc732d159d32d5ead8f623d5811d0f3e5c34c5e
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-03-23 13:21:59 -0700

    fix: allow for no image file

--- components/sections/sectionComponents/Portrait.tsx
+++ components/sections/sectionComponents/Portrait.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { urlForImage } from '@/lib/sanity'
 
-type Props = { image: { asset: { _id: string; url: string; }; }; name: string; title: string; };
+type Props = { image?: { asset: { _id: string; url: string; }; }; name: string; title: string; };
 
 const Portrait = (props: Props) => {
     return (
         <div className="flex flex-col mx-auto mb-12 justify-center items-center basis-1/3">
             <div className="relative w-[60vw] h-[80vw] md:w-[30vw] md:h-[40vw] lg:w-[15vw] lg:h-[20vw] border-2 border-gray-700">
-                <Image
+                {props.image && <Image
                     src={urlForImage(props.image).url()}
                     alt={props.name}
                     fill={true}
@@ -15,7 +15,7 @@ const Portrait = (props: Props) => {
                     sizes='(max-width: 768px) 100vw,
               					(max-width: 1200px) 50vw,
               					33vw'
-                />
+                />}
             </div>
             <h3 className="text-center font-semibold text-xl mt-2 text-pa-navy-4">
                 {props.name}

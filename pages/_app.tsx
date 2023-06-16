1394ce80b1d1a1eac854ed759f0536a0c24e8312
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-06-15 17:22:23 -0700

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { Livvic, Spectral } from 'next/font/google'
 import type { AppProps } from 'next/app'
-
+import { useEffect, useState } from 'react'
 
 const livvic = Livvic({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '900'],
@@ -19,14 +19,23 @@ const spectral = Spectral({
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			
+			<Hydrated>
 				<main className={` ${spectral.variable} ${livvic.variable}`}>
 					<Component {...pageProps} />
 				</main>
-			
+			</Hydrated>
 		</>
 	)
 }
 export default MyApp
 
+const Hydrated = ({ children }: { children?: any }) => {
+	const [hydration, setHydration] = useState(false)
 
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			setHydration(true)
+		}
+	}, [])
+	return hydration ? children : null
+}

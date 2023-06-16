ebf88ebfc134d36cf22156b1e81852f81769d059
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-06-15 19:00:11 -0700

    .

--- studio/schemas/components/bodyPortableText.js
+++ studio/schemas/components/bodyPortableText.js
@@ -41,9 +41,7 @@ export default {
                 title: 'URL',
                 name: 'href',
                 type: 'url',
-                validation: Rule => Rule.uri({
-                  scheme: ['http', 'https', 'mailto', 'tel']
-                })
+                
               }
             ]
           }

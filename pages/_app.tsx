2890bae063dbff28f66408791efb589ebb570c4a
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-11-01 18:45:27 -0700

    fix: fonts and contact form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,19 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
+import { Livvic, Spectral } from 'next/font/google'
+import { useEffect, useState } from 'react'
 import { lazy } from 'react'
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-mono',
+const livvic = Livvic({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '900'],
   subsets: ['latin'],
-  weight: ['500', '700'],
+  variable: '--font-livvic',
 })
-
-const sans = Inter({
-  variable: '--font-sans',
+const spectral = Spectral({
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
+  variable: '--font-spectral',
 })
 
 const PreviewProvider = lazy(() => import('components/preview/PreviewProvider'))
@@ -31,23 +24,55 @@ export default function App({ Component, pageProps }: AppProps) {
   const { draftMode, token } = pageProps
   return (
     <>
-      <style jsx global>
-        {`
-          :root {
-            --font-mono: ${mono.style.fontFamily};
-            --font-sans: ${sans.style.fontFamily};
-            --font-serif: ${serif.style.fontFamily};
-          }
-        `}
-      </style>
-
+      <Hydrated>
+      <main className={` ${spectral.variable} ${livvic.variable}`}>
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
         </PreviewProvider>
       ) : (
         <Component {...pageProps} />
-      )}
+      )}</main>
+      </Hydrated>
+      {/* form for netlify */}
+      <form
+        name='contact'
+        method='POST'
+        data-netlify='true'
+        netlify-honeypot='bot-field'
+        className='hidden'
+      ><input type='hidden' name='form-name' value='contact' /><input
+          type='text'
+          name='first-name'
+          id='first-name'
+          autoComplete='given-name'
+        /><input
+          type='text'
+          name='last-name'
+          id='last-name'
+          autoComplete='family-name'
+        /><input
+          id='email'
+          name='email'
+          type='email'
+          autoComplete='email'
+        /><textarea
+          id='message'
+          name='message'
+          rows={4}
+          defaultValue={''}
+        /></form>
     </>
   )
+}
+
+const Hydrated = ({ children }: { children?: any }) => {
+  const [hydration, setHydration] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHydration(true)
+    }
+  }, [])
+  return hydration ? children : null
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms', '@tailwindcss/typography')],
 }

9ef59ed81898754a4daa4974fb93bd32f37f0cdc
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-11-06 13:39:25 -0800

    chore: clean up blog post filtering

--- components/pages/blog/BlogPage.tsx
+++ components/pages/blog/BlogPage.tsx
@@ -22,30 +22,40 @@ const BlogPage = ({ page, preview, loading }: BlogPageProps) => {
         return post.category[0].title !== "Archived" 
         // && post.category[0].title !== "Past Events";
     });
-    const firstThreePosts = filteredPosts.slice(1, 4)
+
+    const eventPosts = _.filter(filteredPosts, post => post.category[0].title === "Event");
+    const nonEventPosts = _.filter(filteredPosts, post => post.category[0].title !== "Event");
+
+    // Reverse the order of the eventPosts array
+    const reversedEventPosts = _.reverse([...eventPosts]);
+
+    // Merge the reversedEventPosts array and nonEventPosts array
+    const reorderedPosts = [...reversedEventPosts, ...nonEventPosts];
+
+    const firstThreePosts = reorderedPosts.slice(1, 4)
 
     return (
         <>
         <BlogPageHead page={page} />
         <Layout preview={preview} loading={loading}>
-            <main>{console.log(firstThreePosts)}
+            <main>
                 <div className='mx-auto mb-24 flex max-w-[1600px] flex-col justify-center md:flex-row '>
                     <div className='group w-full cursor-pointer bg-white p-4 pb-16 h-full md:m-4 md:w-6/12 md:border-[2px] md:border-pa-navy-4 lg:m-12 lg:w-5/12 lg:p-10'>
-                        <Link href={`/post/${filteredPosts[0].slug.current}`} key={posts[0]._id}>
+                        <Link href={`/post/${reorderedPosts[0].slug.current}`} key={posts[0]._id}>
                             <div className='relative w-full aspect-square'>
-                                    {filteredPosts[0].mainImage && (
+                                    {reorderedPosts[0].mainImage && (
                                     <Image
-                                        src={urlForImage(filteredPosts[0].mainImage)?.url() || ''}
-                                        alt={filteredPosts[0].title}
+                                        src={urlForImage(reorderedPosts[0].mainImage)?.url() || ''}
+                                        alt={reorderedPosts[0].title}
                                         fill={true}
                                         style={{ objectFit: 'contain' }}
                                         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 40vw'
                                     />
                                 )}
                             </div>
-                            <h1 className='mt-10 mb-1 text-3xl group-hover:underline'>{filteredPosts[0].title}</h1>
+                            <h1 className='mt-10 mb-1 text-3xl group-hover:underline'>{reorderedPosts[0].title}</h1>
 
-                            {filteredPosts[0].category.map((c: any, i: number) => (
+                            {reorderedPosts[0].category.map((c: any, i: number) => (
                                 <p
                                     className='text-md'
                                     style={{
@@ -57,28 +67,15 @@ const BlogPage = ({ page, preview, loading }: BlogPageProps) => {
                                 </p>
                             ))}
                             <div className='flex flex-col items-start mt-4'>
-                                <div className='relative w-16 h-16 space-x-2 rounded-full'>
-                                    {filteredPosts[0].author && (
-                                        <Image
-                                            fill={true}
-                                            style={{ objectFit: 'contain' }}
-                                            className=''
-                                            src={urlForImage(filteredPosts[0].author.image)?.url()!}
-                                            alt={filteredPosts[0].author.name}
-                                            sizes='(max-width: 768px) 15vw,
-              					(max-width: 1200px) 15vw,
-              					15vw'
-                                        />
-                                    )}
-                                </div>
+                                
                             </div>
                             <div>
-                                <p>{filteredPosts[0].excerpt}</p>
+                                <p>{reorderedPosts[0].excerpt}</p>
                             </div>
                             <p className='mt-2 underline text-pa-blue-4 underline-offset-2'>Read full story</p>
                         </Link>
                     </div>
-                    <BlogFeaturedPosts posts={filteredPosts} />
+                    <BlogFeaturedPosts posts={reorderedPosts} />
                 </div>
 
                 <h2 className='pt-10 text-2xl text-center text-white text-bold bg-pa-blue-4 sm:text-4xl'>
